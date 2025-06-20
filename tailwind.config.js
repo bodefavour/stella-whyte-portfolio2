0fec8c7f25dd0bb53d64a7fc066a44cb12346083
bodefavour <bodefavour@gmail.coom>
2025-06-20 17:01:40 +0100

    Update Tailwind CSS version and restructure configuration files

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                serif: ["Playfair Display", "serif"],
+                sans: ["Outfit", "sans-serif"],
+            },
+        },
+    },
+    plugins: [],
+}
