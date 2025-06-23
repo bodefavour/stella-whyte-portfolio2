ce3b37105e590661a7a4b2ac0614b37a0f62ddc6
bodefavour <bodefavour@gmail.coom>
2025-06-23 17:24:26 +0100

    add highlisghtsection to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { NavHeroCombo } from "./components/HeroSection";
 import AchievementSection from "./components/achievement";
 import { TimelineCarousel } from "./components/TimelineSection";
 import { AboutSection } from "./components/AboutSection";
+import { HighlightsSection } from "./components/HighlightsSection";
 
 const App: React.FC = () => {
   return (
     <main>
       <NavHeroCombo />
       <AboutSection />
+      <HighlightsSection />
       <TimelineCarousel />
     </main>
   );
