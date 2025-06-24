33a9903ef73aa8389bf51ca4ec93ba9ccd040343
Favour Bode <bodefavour@gmail.com>
2025-06-24 09:50:55 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavHeroCombo } from "./components/HeroSection";
-import AchievementSection from "./components/achievement";
+// import AchievementSection from "./components/achievement";
 import { TimelineCarousel } from "./components/TimelineSection";
 import { AboutSection } from "./components/AboutSection";
 import { HighlightsSection } from "./components/HighlightsSection";
