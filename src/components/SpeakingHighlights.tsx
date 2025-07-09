8e582b2ebcf17eab7ebf5754c80c469961ba76d6
Favour Bode <bodefavour@gmail.com>
2025-07-09 22:35:42 +0100

    Update SpeakingHighlights.tsx

--- src/components/SpeakingHighlights.tsx
+++ src/components/SpeakingHighlights.tsx
@@ -1,5 +1,5 @@
 import Slider from "react-slick";
-import { SpeakingCard } from "./SpeakingCard"; // 👈 import
+import { SpeakingCard } from "./SpeakingCard";
 
 const speakingEvents = [
 {
@@ -18,11 +18,11 @@ const speakingEvents = [
     title: "Africa Trade Consortium’s Tea Break Edition 2025",
     description:
       "Speaking at the event held at the Grand Pela Hotel in Abuja, Okhueleigbe commended the government’s efforts to support businesses through tax exemptions, Pioneer Status Incentives, Export Processing Grants, and other tax reliefs. She noted that these incentives will not only strengthen businesses but also create jobs, reduce poverty, and improve the standard of living.",
-    // coverVideo: "/assets/video-sample.mp4", // optional
+    // coverVideo: "https://res.cloudinary.com/dydwcaxmx/video/upload/v1752093628/VID-20250706-WA0032_ftnclr.mp4", // optional
     coverImage: "/assets/Screenshot_20250525-213124.jpg",
     images: [
       "/assets/IMG-20250709-WA0006.jpg",
-      "/assets/IMG-20250709-WA0007.jpg",
+      "Screenshot_20250525-213124.jpg",
       "/assets/IMG-20250709-WA0008.jpg",
       "/assets/IMG-20250709-WA0009.jpg",
     ],
