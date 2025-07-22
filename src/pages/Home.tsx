ee9f642c97db4a459bf828e38ab59a1dc064ec59
Favour Bode <bodefavour@gmail.com>
2025-07-22 15:10:12 +0100

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,13 +6,15 @@ import { HighlightsSection } from "../components/HighlightsSection";
 import { SpotlightSection } from "../components/SpotlightSection";
 import { ContactSection } from "../components/ContactSection";
 import { SpeakingEvents } from "../components/SpeakingHighlights";
+import { LatestSpeakingModal } from "../components/LatestSpeakingModal";
 
 const Home = () => {
     return (
         <main>
+            <LatestSpeakingModal />
             <NavHeroCombo />
             <AboutSection />
-<SpeakingEvents />
+            <SpeakingEvents />
             <HighlightsSection />
             <TimelineCarousel />
             <SpotlightSection />
